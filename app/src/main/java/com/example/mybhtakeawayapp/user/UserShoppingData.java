96bff96ce91fc23460d5f3c169da75de0ffdfc48
王永瑶 <19374223@buaa.edu.cn>
2022-12-29 13:17:19 +0800

    提交订单

---
+++ app/src/main/java/com/example/mybhtakeawayapp/user/UserShoppingData.java
@@ -0,0 +1,43 @@
+package com.example.mybhtakeawayapp.user;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+public class UserShoppingData {
+    public static HashMap<String ,ArrayList<Integer>> dishes = new HashMap<>();
+    public static String curPid;
+
+    public static void addDish(String pid, int did) {
+        if (!dishes.containsKey(pid)) {
+            dishes.put(pid,new ArrayList<>());
+        }
+        Objects.requireNonNull(dishes.get(pid)).add(did);
+    }
+
+    public static void enterProvider(String pid) {
+        curPid = pid;
+        if (!dishes.containsKey(pid)) {
+            dishes.put(pid,new ArrayList<>());
+        }
+    }
+
+    public static ArrayList<Integer> getDishes() {
+        return dishes.get(curPid);
+    }
+
+    public static void clear(String pid) {
+        if (!dishes.containsKey(pid)) {
+            dishes.put(pid,new ArrayList<>());
+        }
+        else {
+            Objects.requireNonNull(dishes.get(pid)).clear();
+        }
+    }
+
+}
