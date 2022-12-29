dd0e62e933faeb711ba129be75dbe8d60683f6c7
lijinghao <lijinghao@buaa.edu.com>
2022-12-29 16:28:30 +0800

    提交订单部分参数补全

--- app/src/main/java/com/example/mybhtakeawayapp/user/UserActivityOrderEnsure.java
+++ app/src/main/java/com/example/mybhtakeawayapp/user/UserActivityOrderEnsure.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,7 +30,11 @@ import java.util.ArrayList;
 public class UserActivityOrderEnsure extends Activity {
     private Button back;
     private Button pay;
-    private TextView order_day;
+    private Spinner order_day;
+    private Spinner order_time;
+    private TextView order_address;
+    private Spinner order_man;
+    private TextView order_comment;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,6 +42,11 @@ public class UserActivityOrderEnsure extends Activity {
         setContentView(R.layout.user_activity_order_ensure);
         pay = findViewById(R.id.pay);
         back = findViewById(R.id.back);
+        order_day=findViewById(R.id.order_day);
+        order_time=findViewById(R.id.order_time);
+        order_address=findViewById(R.id.order_address);
+        order_man=findViewById(R.id.order_man);
+        order_comment=findViewById(R.id.order_comment);
 
         pay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,6 +68,7 @@ public class UserActivityOrderEnsure extends Activity {
                 startActivity(intent);
             }
         });
+        //TODO 外卖员order_man的监听
     }
 
     private void commitOrder() throws JSONException {
@@ -92,11 +103,11 @@ public class UserActivityOrderEnsure extends Activity {
         requestQueue.add(jsonObjectRequest);
         // todo 获取地址、骑手id、备注
         orderUrl = Local.getLocalIp() + "indent/addIndent";
-        jsonObject2.put("address",address);
+        jsonObject2.put("address",order_address.getText().toString());
         jsonObject2.put("uid",Local.getUserLoginId());
         jsonObject2.put("rider",rider);
         jsonObject2.put("pid",UserShoppingData.curPid);
-        jsonObject2.put("comment",comment);
+        jsonObject2.put("comment",order_comment.getText().toString());
         jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, orderUrl,
                 jsonObject2, new Response.Listener<JSONObject>() {
             @Override
@@ -106,8 +117,9 @@ public class UserActivityOrderEnsure extends Activity {
                     String msg = jsonObject.getString("msg");
                     // todo 设置提交成功
                     if (state) {
-                        saler_name.setText(jsonObject.getString("sellerName"));
-                        saler_income.setText(Double.toString(jsonObject.getDouble("income")));
+                        Toast.makeText(UserActivityOrderEnsure.this, "提交订单成功", Toast.LENGTH_SHORT).show();
+//                        saler_name.setText(jsonObject.getString("sellerName"));
+//                        saler_income.setText(Double.toString(jsonObject.getDouble("income")));
                     } else {
                         Toast.makeText(UserActivityOrderEnsure.this, "提交订单失败", Toast.LENGTH_SHORT).show();
                     }
